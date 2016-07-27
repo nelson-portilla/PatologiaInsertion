3df6840f421123d8cf88fd5d5cc71398f710740b
Nelson Portilla <nelson.herrera@correounivalle.edu.co>
2016-07-27 12:21:33 -0500

    Algunos Archivos formato diferente, revisar el texto plano, limpiar

--- scripts/insertar.py
+++ scripts/insertar.py
@@ -1,7 +1,8 @@
 import os,sys
 #from extraccion as t import *
 import extraccion as extraer
-global matriz
+global matriz 
+import glob
 matriz=[[None] * 6 for i in range(2)]
 
 def insertar():
@@ -17,43 +18,41 @@ def insertar():
 	except Exception, e:
 		print "Ha fallado la insercion: error general",str(e)
 
-	
-
-def crearcsv():
+def crearMatriz(numArchivos):
 	global matriz
-	# matriz[0][0]="id"
-	# matriz[0][1]="historiaclinica"
-	# matriz[0][2]="numeroregistro"
-	# matriz[0][3]="descmacro"
-	# matriz[0][4]="descmicro"
-	# matriz[0][5]="diagnostico"
-	# matriz[0][6]="html"
-
-	# for i in range(1,2):
-	# 	matriz[i][0]='1'
-	# 	matriz[i][1]=extraer.getHistoriaClinica()
-	# 	matriz[i][2]=extraer.getNumeroRegistro()
-	# 	matriz[i][3]=extraer.getMacro()
-	# 	matriz[i][4]=extraer.getMicro()
-	# 	matriz[i][5]=extraer.getDiagnostico()
-	# 	matriz[i][6]=extraer.getHTML()matriz[0][0]="id"
-	
+	matriz=[[None] * 6 for i in range(numArchivos+1)]
 	matriz[0][0]="historiaclinica"
 	matriz[0][1]="numeroregistro"
 	matriz[0][2]="descmacro"
 	matriz[0][3]="descmicro"
 	matriz[0][4]="diagnostico"
 	matriz[0][5]="html"
+	print "==> Creando Matriz ..OK"
+	
+def contarArchivos(ruta):
+	count=2
+	count=len([name for name in os.listdir(ruta) if os.path.isfile(os.path.join(ruta, name))])
+	print "==> Contando Archivos ..OK", count
+	return count
 
-	for i in range(1,2):
-		matriz[i][0]=extraer.getHistoriaClinica()
-		matriz[i][1]=extraer.getNumeroRegistro()
-		matriz[i][2]=extraer.getMacro()
-		matriz[i][3]=extraer.getMicro()
-		matriz[i][4]=extraer.getDiagnostico()
-		matriz[i][5]=extraer.getHTML()
+def crearcsv(i):
 	
-	
+
+	# for i in range(1,len(matriz)+1):
+	# 	matriz[i][0]=extraer.getHistoriaClinica()
+	# 	matriz[i][1]=extraer.getNumeroRegistro()
+	# 	matriz[i][2]=extraer.getMacro()
+	# 	matriz[i][3]=extraer.getMicro()
+	# 	matriz[i][4]=extraer.getDiagnostico()
+	# 	matriz[i][5]=extraer.getHTML()
+	matriz[i][0]=extraer.getHistoriaClinica()
+	matriz[i][1]=extraer.getNumeroRegistro()
+	matriz[i][2]=extraer.getMacro()
+	matriz[i][3]=extraer.getMicro()
+	matriz[i][4]=extraer.getDiagnostico()
+	matriz[i][5]=extraer.getHTML()
+
+def escribirCSV():
 	reg=open("registro.csv", 'w')
 
 	for idx, linea in enumerate(matriz):
@@ -77,6 +76,7 @@ def extraerDatos(ruta):
 	extraer.ArmarJson()
 	#Se Convierte de HTML A TXT
 	extraer.escribirArchivo()
+	objHTML.inicializar()
 	
 
 def crearSQL():
@@ -87,7 +87,23 @@ def crearSQL():
 	
 
 if __name__ == '__main__':
-	extraerDatos("../informes/m10-0002.txt.html")
-	crearcsv()
+	i=1   
+	totalRegistros=contarArchivos("../informes")
+	crearMatriz(totalRegistros)
+	path = '../informes/*.html'   
+	files=glob.glob(path)   
+	for file in files:
+		print "\n==> Enviando archivo: ", file+"..."+str(i)
+		filedata=open(file, 'r').read()
+		extraerDatos(filedata)
+		crearcsv(i)		
+		i+=1
+	escribirCSV()	
 	crearSQL()
 	insertar()
+	
+	
+
+	# extraerDatos("../informes/m10-0002.txt.html")
+	
+	
