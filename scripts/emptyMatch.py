8dd07ec558b50747e0360fa45d345ec4fbe891d0
Nelson Portilla <nelson.herrera@correounivalle.edu.co>
2016-08-09 14:09:47 -0500

    r.004: Nuevas Listas

--- scripts/emptyMatch.py
+++ scripts/emptyMatch.py
@@ -3,11 +3,11 @@ import progressBar as progress
 import subprocess
 
 #Clase de emparejamiento entre master y listas_de_vacios para obtener lista de archivos faltantes.
-def buscar(nombre):
+def buscarenmaster(nombre):
 	try:		
 		salida_system = os.popen('echo | grep -c "'+ nombre +'" ../listaconpesos/master_formateado.txt').read()
 		coincidencias=int(salida_system)
-		out=open('../listas_de_vacios/estan_en_master.txt', 'a')
+		out=open('../listas_de_vacios/SI_estan_en_master.txt', 'a')
 		if coincidencias!=0:
 			out.write(nombre)
 			out.write("\n")
@@ -17,17 +17,52 @@ def buscar(nombre):
 		print "Ha fallado",str(e),nombre
 		sys.exit(1)
 
+def buscarenliliana (nombre) :
+	salida_system = os.popen('echo | grep "'+nombre+'" ../listaconpesos/listadisco1.txt').read()
+	if salida_system!="":
+		# coincidencias=salida_system
+		out=open('../listas_de_vacios/SI_estan_en_disco1.txt', 'a')
+		out.write(salida_system)
+		out.write("\n")
+		# out.close()		
+		flag=True
+	else:
+		flag=False
+	return flag
+
+
+def buscarenerlinda(nombre):
+	salida_system = os.popen('echo | grep "'+ nombre +'" ../listaconpesos/listadisco2.txt').read()
+	if salida_system!="":
+		# coincidencias=salida_system.split()
+		oute=open('../listas_de_vacios/SI_estan_disco2.txt', 'a')
+		oute.write(salida_system)
+		oute.write("\n")
+		oute.close()
+	else:
+		outp=open('../listas_de_vacios/PerdidosDefinitivos.txt', 'a')
+		outp.write(nombre)
+		outp.write("\n")
+		outp.close()
+	
+
+
+
 if __name__ == '__main__':
-	p = subprocess.Popen(['wc', '-l', '../listas_de_vacios/lista_vacios.txt'], stdout=subprocess.PIPE, 
+	p = subprocess.Popen(['wc', '-l', '../listas_de_vacios/Perdidos.txt'], stdout=subprocess.PIPE, 
                                           stderr=subprocess.PIPE)
 	result, err = p.communicate()
 	total_lineas=int(result.strip().split()[0])
 	number=0	
-
-	lista_vacios=open('../listas_de_vacios/lista_vacios.txt', 'r')
+	lista_vacios=open('../listas_de_vacios/Perdidos.txt', 'r')
 	for linea in lista_vacios:
-		buscar(linea[:-6])		
-		progress.printProgress(number, total_lineas, prefix = 'Progress:', suffix = 'Complete', barLength = 50)
+		nombre=str(linea).upper().strip()
+		print nombre
+		if buscarenliliana(nombre):
+			None
+		else:
+			buscarenerlinda(nombre)
+		# progress.printProgress(number, total_lineas-1, prefix = 'Progress:', suffix = 'Complete', barLength = 50)
 		number+=1
 
-	# buscar("m16-739.txt.html")
+	# buscar_en_liliana("m03-6688.txt")
