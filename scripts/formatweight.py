a545585c824e4bfad2546e5ca14fd970cbab16f5
Nelson Portilla <nelson.herrera@correounivalle.edu.co>
2016-08-10 11:26:41 -0500

    R.004: Actualizacion de archivos faltantes

--- scripts/formatweight.py
+++ scripts/formatweight.py
@@ -4,12 +4,14 @@ import os,sys
 #recibe ../../informes-patologia-html/r03/r03-058.txt.html
 def formatear():
 	try:
-		inputtxt=open('../listaconpesos/master.txt', 'r')
-		outputtxt=open('../listaconpesos/master_formateado.txt', 'w')
+		# inputtxt=open('../listaconpesos/master.txt', 'r')
+		inputtxt=open('../listaconpesos/masterhtml.txt', 'r')
+		# outputtxt=open('../listaconpesos/master_formateado.txt', 'w')
+		outputtxt=open('../listaconpesos/master_formateado_html.txt', 'w')
 
 		for linea in inputtxt:
 			linea=linea.rstrip()			
-			if linea[:5]=="-rwxr":
+			if linea[:5]=="-rw-r":
 				print "formating..."								
 				lista=linea.split()			
 				peso=lista[4]				
