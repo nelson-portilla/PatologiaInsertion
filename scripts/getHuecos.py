9b8da08c677bb74784730eb4efb7697b37b35aed
Nelson Portilla <nelson.herrera@correounivalle.edu.co>
2016-08-12 12:00:07 -0500

    R.005: Actualizacion de listas, completas

--- scripts/getHuecos.py
+++ scripts/getHuecos.py
@@ -23,9 +23,25 @@ if __name__ == '__main__':
 	# rutadestino='../lista_de_huecos/lista_en_base_datos.txt'
 	# archivo_nuevo='../lista_de_huecos/huecos_estan_en_master.txt'
 	
-	rutaorigen='../listaconpesos/liliana.csv'
-	rutadestino='../listaconpesos/master_formateado.txt'
-	archivo_nuevo='../lista_de_huecos/liliana_no_estan_en_master.txt'
+	# rutaorigen='../listaconpesos/liliana.csv'
+	# rutadestino='../listaconpesos/master_formateado.txt'
+	# archivo_nuevo='../lista_de_huecos/liliana_no_estan_en_master.txt'
+
+	# rutaorigen='../listaconpesos/erlinda.csv'
+	# rutadestino='../listaconpesos/master_formateado.txt'
+	# archivo_nuevo='../lista_de_huecos/erlinda_no_estan_en_master.txt'
+
+	# rutaorigen='../listaconpesos/listadisco2.txt'
+	# rutadestino='../listaconpesos/master_formateado.txt'
+	# archivo_nuevo='../lista_de_huecos/disco2_no_estan_en_master.txt'
+
+	# rutaorigen='../lista_de_huecos/huecos_estan_en_master.txt'
+	# rutadestino='../listaconpesos/master_formateado_html.txt'
+	# archivo_nuevo='../lista_de_huecos/huecos_estan_en_master_pero_wp.txt'
+
+	rutaorigen='../listaconpesos/master_formateado.txt'
+	rutadestino='../listaconpesos/master_formateado_html.txt'
+	archivo_nuevo='../lista_de_huecos/master_no_estan_html.txt'
 
 	p = subprocess.Popen(['wc', '-l', rutaorigen], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	result, err = p.communicate()
@@ -35,7 +51,17 @@ if __name__ == '__main__':
 	lista_master=open(rutaorigen, 'r')
 
 	for linea in lista_master:
-		nombre=str(linea.split(";")[3].strip().lower())		
+		##PARA LILIANA
+		# nombre=str(linea.split(";")[3].strip().lower())		
+		##PARA ERLINDA
+		# nombre=str(linea.split(";")[2].split()[1].strip().lower())		
+		
+		##PARA DISCO1
+		# if linea.strip()[-1:]!="!":
+		# 	nombre=str(linea.strip().lower())		
+		
+
+		nombre=str(linea.split()[1].strip())		
 		buscarenlista(nombre, rutadestino, archivo_nuevo)
 		progress.printProgress(number, total_lineas-1, prefix = 'Progress:', suffix = 'Complete', barLength = 50)
 		number+=1
